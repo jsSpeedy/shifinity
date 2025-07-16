be713610dc8b3f3b33340f7100574693e397ff4e
Shifu <emreaydin6587@gmail.com>
2025-07-16 20:48:51 +0300

    [FEATURE] GlobalStyles Oluşturuldu.

---
+++ src/styles/globalStyles.js
@@ -0,0 +1,29 @@
+// src/styles/globalStyles.js
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  *, *::before, *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html, body {
+    height: 100%;
+    font-family: "Sora",sans-serif;
+    background-color: ${({ theme }) => theme.background};
+    color: ${({ theme }) => theme.text};
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  button {
+    font-family: inherit;
+    cursor: pointer;
+  }
+`;
+
+export default GlobalStyle;
