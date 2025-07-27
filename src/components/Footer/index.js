ddc010c0e550ed99db07c5b98499fb37b028e787
Shifu <emreaydin6587@gmail.com>
2025-07-27 14:21:01 +0300

    [FEATURE] Footer Yapısı Oluşturuldu.

---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+//src/components/Footer/index.js
+"use client";
+import Link from "next/link";
+import styled from "styled-components";
+
+const Footer = ({ footerData }) => {
+  return <div>FOOTER</div>;
+};
+
+export default Footer;
