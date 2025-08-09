b41ada64f6b2cc4af6222e929578ecaf0a528462
Shifu <emreaydin6587@gmail.com>
2025-08-09 15:05:02 +0300

    [FEATURE] Next Confige remotePatterns eklendi.

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,16 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "media.themoviedb.org",
+        port: "",
+        pathname: "/t/p/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
