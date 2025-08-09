085e6e166f14c7c98bcbf64ee360a17e8eb72d00
Shifu <emreaydin6587@gmail.com>
2025-08-09 14:46:17 +0300

    [FEATURE] Film Sayfaları Oluşturuldu.

---
+++ src/app/movies/[id]/page.js
@@ -0,0 +1,6 @@
+//src/app/movies/[id]/page.js
+"use client";
+
+export default function MoviesDetail() {
+  return <div>MOVİES DETAIL</div>;
+}
---
+++ src/app/movies/page.js
@@ -0,0 +1,6 @@
+//src/app/movies/page.js
+"use client";
+
+export default function Movies() {
+  return <div>MOVİES</div>;
+}
