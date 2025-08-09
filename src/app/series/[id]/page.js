3e751ba156c2f2bce787be30ac6e805cecb32d94
Shifu <emreaydin6587@gmail.com>
2025-08-09 14:47:59 +0300

    [FEATURE] Dizi Sayfaları Oluşturuldu.

---
+++ src/app/series/[id]/page.js
@@ -0,0 +1,6 @@
+//src/app/series/[id]/page.js
+"use client";
+
+export default function SeriesDetail() {
+  return <div>SERİES DETAIL</div>;
+}
---
+++ src/app/series/page.js
@@ -0,0 +1,6 @@
+// src/app/series/page.js
+"use client";
+
+export default function Series() {
+  return <div>SERİES</div>;
+}
